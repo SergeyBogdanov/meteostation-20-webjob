44f44d7f7c409091e482726affe1dd3de2e34f0a
Sergey Bogdanov <Sergey.Bogdanov@mir.dev>
2024-07-31 17:04:06 +0300

    Simple web app

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+
+function returnSimpleText(req, res, next) {
+    res.send(`This is just a stub site. This message is generated at [${Date.now()}]`);
+}
+
+// Redirect requests to the public subdirectory to the root
+const app = express();
+app.get('/', returnSimpleText);
+app.use((req, res /* , next */) => {
+  res.redirect('/');
+});
+
+const server = http.createServer(app);
+server.listen(process.env.PORT || '3000', () => {
+  console.log('Listening on %d.', server.address().port);
+});
