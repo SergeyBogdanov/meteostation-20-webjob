66af50bd4a064192efe17fedb6c512d8bb8ecaa8
Sergey Bogdanov <Sergey.Bogdanov@mir.dev>
2024-08-01 14:49:10 +0300

    Use express directly

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use((req, res /* , next */) => {
   res.redirect('/');
 });
 
-const server = http.createServer(app);
-server.listen(process.env.PORT || '3000', () => {
-  console.log('Listening on %d.', server.address().port);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('Listening on %d.', port);
 });
+//const server = http.createServer(app);
+//server.listen(process.env.PORT || '3000', () => {
+//  console.log('Listening on %d.', server.address().port);
+//});
